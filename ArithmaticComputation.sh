55528d7952de6bc56fd55c4237595fb9596d9320
sheetalc818 <sheetalc818@gmail.com>
2020-01-08 10:14:48 +0530

    Added : Storing Results into the Dictionary

--- ArithmaticComputation.sh
+++ ArithmaticComputation.sh
@@ -2,18 +2,28 @@
 
 echo "======Arithmetic Computation & Sorting====="
 
+#Dictionary
+declare -A Computation
+
 #Get Three Input from user
 echo "Enter Three Values :"
 read a b c
 
-#Compute a + b * c
+#Compute a + b * c and storing data into Dictionary
 result=$(( $a+$b*$c ))
+Computation[$((counter++))]=$result
 
-#Compute a * b + c
+#Compute a * b + c and storing data into Dictionary
 result=$(( $a*$b+$c ))
+Computation[$((counter++))]=$result
 
-#Compute c + a / b
+#Compute c + a / b and storing data into Dictionary
 result=$(( $c+$a/$b ))
+Computation[$((counter++))]=$result
 
-#Compute a % b + c
+#Compute a % b + c and storing data into Dictionary
 result=$(( $a%$b+$c ))
+Computation[$((counter++))]=$result
+
+echo  "All results of Computation" ${Computation[@]}
+
